d104a03a5bac49aa1f8bd66eaae78b74fd136fec
Philip Trauner <philip.trauner@arztpraxis.io>
2018-08-01 22:28:26 +0200

    Switched to Python based package dump

--- dump/debian/dump.sh
+++ dump/debian/dump.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 set -e
+set -x
 
-./dump/debian/dump-installed-packages.sh
+./dump/debian/dump-installed-packages.py
 ./dump/debian/dump-scripts.sh
 ./dump/debian/dump-user-config.sh
 ./dump/debian/dump-dotfiles.sh
