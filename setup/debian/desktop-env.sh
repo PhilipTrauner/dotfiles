e1dd5c7f04ebb4e44ee74578e6651e7de55012b4
Philip Trauner <philip.trauner@arztpraxis.io>
2018-08-01 22:48:21 +0200

    Moved desktop/lightdm background change to 'alternatives.sh'

--- setup/debian/desktop-env.sh
+++ setup/debian/desktop-env.sh
@@ -7,8 +7,6 @@ export PATH="$HOME/.yarn/bin:$PATH"
 # Set i3 style
 i3-style flat-gray -o ~/.config/i3/config --reload
 
-# Set desktop/lightdm background
-sudo update-alternatives --install /usr/share/images/desktop-base/desktop-background desktop-background /usr/share/images/desktop-base/background.svg 100
 
 # Set gtk theme
 gtk-theme-switch2 /usr/share/themes/Numix
