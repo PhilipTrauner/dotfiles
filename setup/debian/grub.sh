c4cc5dd81a8a75a570fcce3f1fa72c715132181c
Philip Trauner <philip.trauner@arztpraxis.io>
2018-08-01 22:29:34 +0200

    Moved grub configuration into 'alternatives.sh' and 'setup.sh'

--- setup/debian/grub.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-sudo update-alternatives --install /usr/share/images/desktop-base/desktop-grub.png desktop-grub /usr/share/images/desktop-base/background.png 100
-sudo update-grub
