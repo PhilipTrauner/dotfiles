b497fd7a5bbc3ed2f5c5363b0c3441c1ee26df75
Philip Trauner <philip.trauner@arztpraxis.io>
2018-08-01 22:36:21 +0200

    Install pipenv locally and moved Alacritty installation to 'git-repos.sh'

--- setup/debian/custom-package-managers.sh
+++ setup/debian/custom-package-managers.sh
@@ -2,11 +2,5 @@
 
 set -e
 
-export PATH="$HOME/.cargo/bin:$PATH"
-
-if ! [ -x "$(command -v alacritty)" ]; then
-	cargo install --git https://github.com/jwilm/alacritty --branch scrollback
-fi
-
-sudo pip3 install pipenv
+pip3 install pipenv --user
 yarn global add i3-style
