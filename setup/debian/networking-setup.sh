878122dc76bcea903b95cb88a1e537351fdb869f
Philip Trauner <philip.trauner@arztpraxis.io>
2018-08-13 02:30:40 +0200

    Added wireless networking setup

--- setup/debian/networking-setup.sh
+++ setup/debian/networking-setup.sh
@@ -4,9 +4,17 @@ set -e
 
 apt-get remove -y ifupdown || echo "ifupdown already removed!"
 
-cat << EOF > /etc/systemd/network/dhcp.network
+cat << EOF > /etc/systemd/network/wired.network
 [Match]
 Name=en*
+
+[Network]
+DHCP=yes
+EOF
+
+cat << EOF > /etc/systemd/network/wireless.network
+[Match]
+Name=wl*
 
 [Network]
 DHCP=yes
