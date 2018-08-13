9d404ce896b00082d47d2979b738396c031dd2dc
Philip Trauner <philip.trauner@arztpraxis.io>
2018-08-13 02:31:47 +0200

    Installed Poetry

--- setup/debian/custom-package-managers.sh
+++ setup/debian/custom-package-managers.sh
@@ -2,5 +2,12 @@
 
 set -e
 
-pip3 install pipenv --user
+export PATH="$HOME/.local/bin:$PATH"
+
+pip3 install pipenv poetry --user
+
+mkdir -p ~/.antigen/completions
+
+poetry completions zsh > ~/.antigen/completions/_poetry
+
 yarn global add i3-style
