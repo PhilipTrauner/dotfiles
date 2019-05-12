aaadb565a8bb6cd38de97ad01b1f5c2e5a4fcb90
Philip Trauner <philip.trauner@arztpraxis.io>
2019-05-12 17:11:53 +0200

    Install 'poetry' and generate shell completions

--- setup/shared/setup.sh
+++ setup/shared/setup.sh
@@ -9,7 +9,7 @@ git lfs install
 mkdir -p ~/.antigen
 curl -L git.io/antigen > ~/.antigen/antigen.zsh
 
-export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$HOME/.poetry/bin:$PATH"
 
 # Install rustup
 if ! [ -x "$(command -v rustup)" ]; then
@@ -19,7 +19,15 @@ if ! [ -x "$(command -v rustup)" ]; then
 	rm rustup.sh
 fi
 
+# Install poetry
+if ! [ -x "$(command -v poetry)" ]; then
+	wget -O poetry.py https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py
+	yes "n" | python get-poetry.py --preview
+	rm poetry.py
+fi
+
 rustup self update
 
 mkdir -p ~/.antigen/completions
 rustup completions zsh > ~/.antigen/completions/_rustup
+poetry completions > ~/.antigen/completions/_poetry
